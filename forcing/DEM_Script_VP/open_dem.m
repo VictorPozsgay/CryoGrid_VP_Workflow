d272febb4a6ded195feb5cba6cec6b67ff22f3f3
VictorPozsgay <victor.pozsgay@gmail.com>
2026-08-07 09:32:36 +0200

    new git ignore

--- forcing/DEM_Script_VP/open_dem.m
+++
@@ -1,5 +0,0 @@
-root_folder = fullfile("..\Forcing_Data\DEM_IGN\raw");
-dem_folder = fullfile("..\Forcing_Data\DEM_IGN");
-shapefile_folder = fullfile("..\Forcing_Data\meteo\safran\shapefile");
-
-preprocess_all_massifs(dem_folder, shapefile_folder)
--- forcing/Forcing_Script_VP/run_prepare_forcing.m
+++
@@ -1,5 +0,0 @@
-era_path = "..\..\forcing\Forcing_Data\meteo\era5";
-safran_path = "..\..\forcing\Forcing_Data\meteo\safran_new";
-out_path = "..\..\forcing\Forcing_Data\meteo\CryoGrid_ready_new";
-
-prepare_forcing_safran(era_path, safran_path, out_path)
